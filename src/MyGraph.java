fa94922dd73af315b9fa2e2d63d37db4117e3bc0
jonathan221483 <captainberkut@gmail.com>
2023-06-01 00:53:56 +0600

    class MyGraph corrected

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,13 +1,13 @@
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-public class MyGraph <Vertex>{
-    private Map<Vertex, List<Vertex>> list;
-    public MyGraph() {
-        list = new HashMap<>();
-    }
-    public void addVertex(Vertex vertex) {
-        list.put(vertex, new LinkedList<>());
+public class MyGraph{
+    private int VertexNumber;
+    private LinkedList<Integer>[] list;
+    public MyGraph(int VertexNumber) {
+        this.VertexNumber = VertexNumber;
+        list = new LinkedList[VertexNumber];
+        for(int i = 0; i < VertexNumber; i++)
+        {
+            list[i] = new LinkedList<>();
+        }
     }
 }
